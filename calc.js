3c908bdc6a101c14322eee67e10e34dd2d3745ed
MackTay <191326787+MackTay@users.noreply.github.com>
2025-03-14 11:16:46 +0900

    Add js that alters button colors on mousedown and up

--- calc.js
+++ calc.js
@@ -1,11 +1,21 @@
 // calc.js
 
-const btn = document.querySelectorAll("button");
+const rows = document.querySelectorAll(".row");
 
-btn.addEventListener("mousedown", (event) => {
-    event.target.style.backgroundColor = "blue";
+rows.forEach(row => {
+    row.addEventListener('mousedown', (event) => {
+        event.target.setAttribute("style", `
+            background-color: lightgray;
+            border: 2px solid black;
+            box-shadow: 0px 0px 0px 0px;
+            `);
 });
 
-btn.addEventListener("mouseup", function() {
-    this.style.backgroundColor = "#f0f0f0";
+    row.addEventListener("mouseup", (event) => {
+        event.target.setAttribute("style", `
+            background-color: #f0f0f0;
+            border: 1px solid black;
+            box-shadow: 2px 2px 2px 1px rgb(0 0 0 / 20%);
+            `);
+});
 });
